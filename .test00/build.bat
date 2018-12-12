1b926bb097383330a0fd6b9f3f473497c40db4dd
Ray Zhang <41682958+zhang-ray@users.noreply.github.com>
2018-12-12 18:33:03 +0800

    +.test00/run.bat

--- .test00/build.bat
+++ .test00/build.bat
@@ -1,5 +1,5 @@
 CALL "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
-mkdir build
-cd build
+mkdir test00-build
+cd test00-build
 cmake -G "Visual Studio 14 2015" ..\.test00
 msbuild "/m" "/p:Configuration=Release" "/consoleloggerparameters:ErrorsOnly" test00.sln
