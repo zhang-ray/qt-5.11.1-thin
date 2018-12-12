17445986bdc69a6c0224f1db93e153de73f17d5a
Ray Zhang <41682958+zhang-ray@users.noreply.github.com>
2018-12-12 17:41:17 +0800

    fix some typo

--- .test00/build.bat
+++ .test00/build.bat
@@ -1,5 +1,5 @@
 CALL "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
 mkdir build
 cd build
-"C:\Program Files\CMake\bin\cmake.exe" -G "Visual Studio 14 2015" ..\.test00
+cmake -G "Visual Studio 14 2015" ..\.test00
 msbuild "/m" "/p:Configuration=Release" "/consoleloggerparameters:ErrorsOnly" test00.sln
