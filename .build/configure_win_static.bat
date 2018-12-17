664bb56351fe14782444e8598f9bad0f6e6f944b
Ray Zhang <41682958+zhang-ray@users.noreply.github.com>
2018-12-17 10:13:43 +0800

    use  "-strip -optimize-size "

--- .build/configure_win_static.bat
+++ .build/configure_win_static.bat
@@ -1,3 +1,3 @@
 CALL "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
 
-configure -confirm-license -opensource -static -platform win32-msvc -mp -release   -strip                 -nomake examples -nomake tests           -no-feature-accessibility -no-feature-appstore-compliant -no-feature-big_codecs -no-feature-calendarwidget -no-feature-codecs -no-feature-commandlineparser -no-feature-completer -no-feature-gestures -no-feature-paint_debug
+configure -confirm-license -opensource -static -platform win32-msvc -mp -release   -strip -optimize-size               -nomake examples -nomake tests           -no-feature-accessibility -no-feature-appstore-compliant -no-feature-big_codecs -no-feature-calendarwidget -no-feature-codecs -no-feature-commandlineparser -no-feature-completer -no-feature-gestures -no-feature-paint_debug
