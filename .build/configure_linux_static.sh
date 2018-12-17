e3cd7601da1311be6e65a09411f39bba0b149f22
Ray Zhang <41682958+zhang-ray@users.noreply.github.com>
2018-12-17 15:19:25 +0800

    [linux] +configure_linux_static.sh

---
+++ .build/configure_linux_static.sh
@@ -0,0 +1,1 @@
+./configure -confirm-license -opensource -static -release -opengl desktop    -nomake examples -nomake tests           -no-feature-appstore-compliant -no-feature-big_codecs -no-feature-calendarwidget -no-feature-codecs -no-feature-commandlineparser -no-feature-completer -no-feature-gestures -no-feature-paint_debug
